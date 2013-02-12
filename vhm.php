c80850d22033801f383e6db186c48a801599f93b
Jason Davis <jason.davis.fl@gmail.com>
2013-02-12 13:29:27 -0500

    Changed Portuguese to English

--- vhm.php
+++ vhm.php
@@ -5,31 +5,31 @@
  * defined by PHP for some reason
  */
 if(!defined("STDIN")) {
-	define("STDIN", fopen('php://stdin','r'));
+    define("STDIN", fopen('php://stdin','r'));
 }
 
 
 /*
- * Checa se vem argumentos
+ * Checks if the next arguments
  */
 if ( preg_match( '/^\-\-php\=/' , $argv[1] ) ) {
-	array_shift($argv);
+    array_shift($argv);
 }
 else if (count($argv) == 0) exit;
 
 
 /*
- * Checa se existe a pasta ~/.vhm
+ * Checks if the folder exists ~/.vhm
  */
 
 /*
- * Le os dados do ~/.vhm/conf @TODO
+ * Le data ~/.vhm/conf @TODO
  */
 
 
 
 /*
- * Variaveis de Configuração
+ * Configuration Variables
  */
 define("APACHE_PATH", '/Applications/MAMP/conf/apache');
 define("APACHE_PATH_SITES", APACHE_PATH . '/sites-enabled/');
@@ -39,24 +39,24 @@ define("APACHE_PORT", 80);
 
 
 /*
- * MAC - Checa se existe a pasta "sites-enabled" e o include no "httpd.conf"
+ * MAC - Checks if there is a folder "sites-enabled" and include the "httpd.conf"
  */
 if (PHP_OS == 'Darwin') {
-	// "sites-enabled"
-	if ( !file_exists( APACHE_PATH_SITES ) ) {
-		mkdir( APACHE_PATH_SITES , 0755 );
-	}
-	
-	// Include da pasta com os vhosts
-	$includeVhosts = "NameVirtualHost *:" . APACHE_PORT . "\n\n";
-	$includeVhosts .= "# Include the virtual host configuration\n";
-	$includeVhosts .= "Include " . APACHE_PATH_SITES . "\n\n";
-	
-	$conf = file_get_contents( APACHE_PATH . '/httpd.conf' );
+    // "sites-enabled"
+    if ( !file_exists( APACHE_PATH_SITES ) ) {
+        mkdir( APACHE_PATH_SITES , 0755 );
+    }
+    
+    // Include folder with vhosts
+    $includeVhosts = "NameVirtualHost *:" . APACHE_PORT . "\n\n";
+    $includeVhosts .= "# Include the virtual host configuration\n";
+    $includeVhosts .= "Include " . APACHE_PATH_SITES . "\n\n";
+    
+    $conf = file_get_contents( APACHE_PATH . '/httpd.conf' );
 
-	if ( !preg_match( "/ˆInclude " . preg_replace("/\//", "\/", APACHE_PATH_SITES) . "$/", $conf ) ) {
-		file_put_contents( APACHE_PATH . '/httpd.conf', $conf . $includeVhosts );
-	}
+    if ( !preg_match( "/ˆInclude " . preg_replace("/\//", "\/", APACHE_PATH_SITES) . "$/", $conf ) ) {
+        file_put_contents( APACHE_PATH . '/httpd.conf', $conf . $includeVhosts );
+    }
 }
 
 
@@ -69,28 +69,28 @@ $feedback = "";
  * Se for nw | del | up
  */
 switch ($argv[1]) {
-	/*
-	 * Cria o vhost
-	 */
-	case 'nw':
-	case 'new':	
-		new_vhost($argv[2]);
-	break;
-	
-	/*
-	 * Deleta o vhost
-	 */
-	case 'del':
-	case 'delete':
-		delete_vhost($argv[2]);
-	break;
-	
-	/*
-	 * Restarta o Apache
-	 */
-	case 'up':
-		restart_apache();
-	break;
+    /*
+     * Create vhost
+     */
+    case 'nw':
+    case 'new': 
+        new_vhost($argv[2]);
+    break;
+    
+    /*
+     * Delete vhost
+     */
+    case 'del':
+    case 'delete':
+        delete_vhost($argv[2]);
+    break;
+    
+    /*
+     * Restart Apache
+     */
+    case 'up':
+        restart_apache();
+    break;
 }
 
 /*
@@ -102,120 +102,120 @@ echo $feedback;
 /**
  * Cria o novo Virtual Host.
  */
-function new_vhost( $nome = NULL ) {
-	
-	$nome = strtolower($nome);
-	
-	/*
-	 * Cria a pasta no WWW do Apache
-	 */
-	$vhPath = APACHE_PATH_WWW . $nome;
-	if ( !file_exists( $vhPath ) ) {
-		mkdir( $vhPath , 0755 );
-	}
-	
-	/*
-	 * Configuração do VH
-	 */
-	$vhConf = "<VirtualHost *:" . APACHE_PORT . ">";
-	$vhConf .= "\n\tDocumentRoot " . $vhPath;
-	$vhConf .= "\n\tServerName localhost." . $nome;
-	$vhConf .= "\n</VirtualHost>"; 
-		
-	/*
-	 * Escreve o arquivo de configuração do VH
-	 */
-	sudo_write_file( APACHE_PATH_SITES . $nome , $vhConf);
-	
-	
-	/*
-	 * Le o arquivo de configuração do HOSTS
-	 */
-	$hConf = file_get_contents( HOSTS_PATH );
-	
-	/*
-	 * Checa a existencia
-	 */
-	$hTitulo = strtoupper($nome);
-	if ( !preg_match( "/" . $hTitulo . "/" , $hConf ) ) {
-		/*
-		 * Cria o dominio
-		 */
-		hosts_configuration($hConf, $hTitulo, $nome);
-		
-		/*
-		 * Escreve o arquivo de configuração do HOSTS
-		 */
-		sudo_write_file( HOSTS_PATH , $hConf );
-	}
-	
-	/*
-	 * Cria o index.html
-	 */
-	$htmIndex = $vhPath . '/index.php';
-	if (!file_exists($htmIndex)) {
-		file_put_contents( $htmIndex , '<h1>Virtual Host criado com VHM !!!</h1>' );
-	}
-	
-	/*
-	 * Restarta o Apache
-	 */
-	restart_apache();
-	
-	/*
-	 * Feedback do proceso
-	 */
-	global $feedback;
-	$feedback .= "\n+-----------------------------------------------+\n";
-	$feedback .= "|           Dados do Virtual Host               |\n";
-	$feedback .= "+-----------------------------------------------+\n";
-	$feedback .= " DocumentRoot: $vhPath\n";
-	$feedback .= "          URL: http://localhost.$nome\n";
-	$feedback .= "+------------------------------------------+\n\n";
+function new_vhost( $name = NULL ) {
+    
+    $name = strtolower($name);
+    
+    /*
+     * Create a folder on the Apache WWW
+     */
+    $vhPath = APACHE_PATH_WWW . $name;
+    if ( !file_exists( $vhPath ) ) {
+        mkdir( $vhPath , 0755 );
+    }
+    
+    /*
+     * Configuration VH
+     */
+    $vhConf = "<VirtualHost *:" . APACHE_PORT . ">";
+    $vhConf .= "\n\tDocumentRoot " . $vhPath;
+    $vhConf .= "\n\tServerName localhost." . $name;
+    $vhConf .= "\n</VirtualHost>"; 
+        
+    /*
+     * Writes the configuration file VH
+     */
+    sudo_write_file( APACHE_PATH_SITES . $name , $vhConf);
+    
+    
+    /*
+     * Reads the configuration file HOSTS
+     */
+    $hConf = file_get_contents( HOSTS_PATH );
+    
+    /*
+     * Check existence
+     */
+    $hTitle = strtoupper($name);
+    if ( !preg_match( "/" . $hTitle . "/" , $hConf ) ) {
+        /*
+         * or domain
+         */
+        hosts_configuration($hConf, $hTitle, $name);
+        
+        /*
+         * Writes the configuration file HOSTS
+         */
+        sudo_write_file( HOSTS_PATH , $hConf );
+    }
+    
+    /*
+     * Creates index.html
+     */
+    $htmIndex = $vhPath . '/index.php';
+    if (!file_exists($htmIndex)) {
+        file_put_contents( $htmIndex , '<h1>Virtual Host criado com VHM !!!</h1>' );
+    }
+    
+    /*
+     * Restart Apache
+     */
+    restart_apache();
+    
+    /*
+     * Feedback do proceso
+     */
+    global $feedback;
+    $feedback .= "\n+-----------------------------------------------+\n";
+    $feedback .= "|           Dados do Virtual Host               |\n";
+    $feedback .= "+-----------------------------------------------+\n";
+    $feedback .= " DocumentRoot: $vhPath\n";
+    $feedback .= "          URL: http://localhost.$name\n";
+    $feedback .= "+------------------------------------------+\n\n";
 }
 
 
 /**
- * Apaga o Virtual Host @TODO
+ * Turn off or Virtual Host @TODO
  */
-function delete_vhost( $nome = NULL ) {
-	
-	$nome = strtolower($nome);
-	
-	/*
-	 * Apaga configuração do VH
-	 */
-	sudo_remove_file( APACHE_PATH_SITES . $nome );
-	
-	/*
-	 * Apaga configuração do HOSTS
-	 */
-	
+function delete_vhost( $name = NULL ) {
+    
+    $name = strtolower($name);
+    
+    /*
+     * Clears the configuration VH
+     */
+    sudo_remove_file( APACHE_PATH_SITES . $name );
+    
+    /*
+     * Clears configuration HOSTS
+     */
+    
 }
 
 
 /**
- * Restarta o Apache
+ * Restart Apache
  */
 function restart_apache() {
-	switch (PHP_OS) {
-		case "Linux":
-			system( "sudo service apache2 restart", $result );
-		break;
-		case "Darwin":
-			system( "/Applications/MAMP/Library/bin/apachectl restart", $result );
-		break;
-	}
+    switch (PHP_OS) {
+        case "Linux":
+            system( "sudo service apache2 restart", $result );
+        break;
+        case "Darwin":
+            system( "/Applications/MAMP/Library/bin/apachectl restart", $result );
+        break;
+    }
 }
 
 
-function hosts_configuration( &$hConf, $hTitulo, $nome ) {
-	
-	/*
-	 * Cria o dominio
-	 */
-	$hConf .= "\n\n## " . $hTitulo . " ##\n";
-	$hConf .= "127.0.0.1	localhost." . $nome;
+function hosts_configuration( &$hConf, $hTitle, $name ) {
+    
+    /*
+     * Cria o domain
+     */
+    $hConf .= "\n\n## " . $hTitle . " ##\n";
+    $hConf .= "127.0.0.1    localhost." . $name;
 }
 
 /**
@@ -223,7 +223,7 @@ function hosts_configuration( &$hConf, $hTitulo, $nome ) {
  */
 function sudo_write_file( $path, $text) {
 
-	exec( "echo '$text' | sudo tee $path", $result );
+    exec( "echo '$text' | sudo tee $path", $result );
 
 }
 
@@ -231,7 +231,7 @@ function sudo_write_file( $path, $text) {
  * Apaga arquivo com permissão root
  */
 function sudo_remove_file( $path ) {
-	
-	exec( "sudo rm -rf $path", $result );
-	
+    
+    exec( "sudo rm -rf $path", $result );
+    
 }
